2f373a037f92df7daa02f4e56255c3262f659c8a
Joseph Ugiagbe <joetalker@Josephs-MBP.attlocal.net>
2023-12-29 17:19:03 -0600

    deployed url

--- internalAPI/FetchAllProducts/index.jsx
+++ internalAPI/FetchAllProducts/index.jsx
@@ -5,7 +5,7 @@ const queryClient = new QueryClient();
 
 export const fetchAllProducts = async () => {
   const domainUrl =
-    "https://eaglecollections-m746jyjio-blessedjoe2000.vercel.app";
+    "https://eaglecollections-ey6v00y1m-blessedjoe2000.vercel.app/";
   const response = await axios.get(`${domainUrl}/api/product`);
   if (response.status !== 200) {
     throw new Error("Cannot fetch data");
