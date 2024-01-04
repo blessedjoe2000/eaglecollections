eb45783267d33919234c2651bfc971659c808f1e
Joseph Ugiagbe <joetalker@Josephs-MBP.attlocal.net>
2024-01-04 10:51:01 -0600

    cors added in config

--- components/AllProducts/AllProducts.jsx
+++ components/AllProducts/AllProducts.jsx
@@ -33,7 +33,7 @@ export default function AllProducts() {
     addToFavorite(productId);
   };
 
-  if (!data) {
+  if (!data.length) {
     return (
       <div className="bg-white mx-5 text-center py-10 ">
         <h1 className="font-bold py-2 text-lg">No products available</h1>
