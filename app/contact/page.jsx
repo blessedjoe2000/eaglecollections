a28a0cb59a4adb3f449556b2a6134e6c46e08d6d
Joseph Ugiagbe <joetalker@Josephs-MBP.attlocal.net>
2024-01-15 19:46:59 -0600

    social media link in a new tab

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -251,15 +251,22 @@ export default function Contact() {
               <SocialIcon
                 network="facebook"
                 url="https://www.facebook.com/profile.php?id=100064705848755&mibextid=ZbWKwL"
+                target="_blank"
               />
               <SocialIcon
                 network="instagram"
                 url="https://www.instagram.com/eaglecollectionstore?igsh=MWhqZDU3M2F3OGx5dQ%3D%3D&utm_source=qr"
+                target="_blank"
               />
-              <SocialIcon network="whatsapp" url="https://wa.me/12103109644" />
+              <SocialIcon
+                network="whatsapp"
+                url="https://wa.me/12103109644"
+                target="_blank"
+              />
               <SocialIcon
                 network="tiktok"
                 url="https://www.tiktok.com/@eaglecollections_store?_t=8j44epTfpTq&_r=1"
+                target="_blank"
               />
             </div>
           </div>
