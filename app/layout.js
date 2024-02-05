1c573d2f02e121b456f35ec459596a18c322100b
Joseph Ugiagbe <joetalker@Josephs-MBP.attlocal.net>
2024-02-05 01:53:53 -0600

    AI customer assistant

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import { CartContextProvider } from "@/components/providers/CartContext/CartCont
 import AuthProvider from "@/components/providers/AuthProvider/AuthProvider";
 import { SearchProvider } from "@/components/providers/SearchProvider/SearchProvider";
 import { Toaster } from "react-hot-toast";
+import VoiceflowAI from "@/components/VoiceflowAI/VoiceflowAI";
 
 export const metadata = {
   title: "Eagle Collections African Fabrics Store",
@@ -14,19 +15,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en " className="bg-light-grey font-muktaFont text-dark-green">
-      <body className="flex flex-col min-h-screen">
-        <Toaster position="top-right" />
-        <AuthProvider>
-          <CartContextProvider>
-            <SearchProvider>
-              <Navbar />
-              <div className="flex-grow">{children}</div>
-              <Footer />
-            </SearchProvider>
-          </CartContextProvider>
-        </AuthProvider>
-      </body>
-    </html>
+    <>
+      <html lang="en " className="bg-light-grey font-muktaFont text-dark-green">
+        <body className="flex flex-col min-h-screen">
+          <Toaster position="top-right" />
+          <AuthProvider>
+            <CartContextProvider>
+              <SearchProvider>
+                <Navbar />
+                <div className="flex-grow">{children}</div>
+                <Footer />
+              </SearchProvider>
+            </CartContextProvider>
+          </AuthProvider>
+          <VoiceflowAI />
+        </body>
+      </html>
+    </>
   );
 }
