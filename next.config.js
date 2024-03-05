4d030333c9d51d2eca2b0dc611a0a2e86a14049a
Joseph Ugiagbe <joetalker@Josephs-MBP.attlocal.net>
2024-03-05 12:33:33 -0600

    eaglecollectionstore added to next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["eaglecollections.s3.amazonaws.com", "lh3.googleusercontent.com"],
+    domains: [
+      "eaglecollections.s3.amazonaws.com",
+      "lh3.googleusercontent.com",
+      "eaglecollectionstore.s3.amazonaws.com",
+    ],
   },
 };
 
