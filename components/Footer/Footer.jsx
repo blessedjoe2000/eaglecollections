91de85048b48aa80ae7f5051f6e8f801a164d91e
blessedjoe2000 <blessedjoe2000@yahoo.com>
2025-01-27 11:44:47 -0600

    footer responsive on all devices

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -10,51 +10,104 @@ export default function Footer() {
   return (
     <footer className="bg-dark-blue text-white ">
       <div className="flex flex-col">
-        <div className="sm:flex justify-around items-center gap-5  p-5 font-poppinsFont text-sm uppercase font-semibold">
-          <div className="flex justify-center items-center">
+        <div className="md:flex-row flex-col flex justify-around items-center gap-5 p-5 text-sm ">
+          <div className="">
             <Logo />
           </div>
-          <div className="flex flex-col justify-center items-center  ">
-            <Link href={"/george"} className="menuitems">
-              <MenuButton>Georges</MenuButton>
-            </Link>
-            <Link href={"/lace"} className="menuitems">
-              <MenuButton>Laces</MenuButton>
-            </Link>
-            <Link href={"/bag"} className="menuitems">
-              <MenuButton>Bags</MenuButton>
-            </Link>
-            <Link href={"/accessories"} className="menuitems">
-              <MenuButton>Accessories</MenuButton>
-            </Link>
+          <div className="flex justify-around items-center  xs:gap-5 sm:gap-20 md:gap-5 lg:gap-10 xl:gap-20">
+            <div className="flex flex-col justify-center items-center  ">
+              <Link href={"/george"} className="menuitems">
+                <MenuButton>Georges</MenuButton>
+              </Link>
+              <Link href={"/lace"} className="menuitems">
+                <MenuButton>Laces</MenuButton>
+              </Link>
+              <Link href={"/headtie"} className="menuitems">
+                <MenuButton>Headtie Gele</MenuButton>
+              </Link>
+              <Link href={"/accessories"} className="menuitems">
+                <MenuButton>Accessories</MenuButton>
+              </Link>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <Link href={"/slippers"} className="menuitems">
+                <MenuButton>Slippers</MenuButton>
+              </Link>
+              <Link href={"/dress"} className="menuitems">
+                <MenuButton>Gown Dresses</MenuButton>
+              </Link>
+              <Link href={"/jewelry"} className="menuitems">
+                <MenuButton>Jewelries</MenuButton>
+              </Link>
+              <Link href={"/bag"} className="menuitems">
+                <MenuButton>Bags</MenuButton>
+              </Link>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <Link href={"/men"} className="menuitems">
+                <MenuButton>Men Shop</MenuButton>
+              </Link>
+              <Link href={"/ankara"} className="menuitems">
+                <MenuButton>Ankara</MenuButton>
+              </Link>
+              <Link href={"/shoe"} className="menuitems">
+                <MenuButton>Shoes</MenuButton>
+              </Link>
+              <Link href={"/clutch"} className="menuitems">
+                <MenuButton>Clutches</MenuButton>
+              </Link>
+            </div>
           </div>
-          <div className="flex flex-col justify-center items-center">
-            <Link href={"/slippers"} className="menuitems">
-              <MenuButton>Slippers</MenuButton>
-            </Link>
-            <Link href={"/ankara"} className="menuitems">
-              <MenuButton>Ankara</MenuButton>
-            </Link>
-            <Link href={"/shoe"} className="menuitems">
-              <MenuButton>Shoes</MenuButton>
-            </Link>
-            <Link href={"/jewelry"} className="menuitems">
-              <MenuButton>Jewelries</MenuButton>
-            </Link>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <Link href={"/men"} className="menuitems">
-              <MenuButton>Men Shop</MenuButton>
-            </Link>
-            <Link href={"/clutch"} className="menuitems">
-              <MenuButton>Clutches</MenuButton>
-            </Link>
-            <Link href={"/dress"} className="menuitems">
-              <MenuButton>Gown Dresses</MenuButton>
-            </Link>
-            <Link href={"/headtie"} className="menuitems">
-              <MenuButton>Headtie Gele</MenuButton>
-            </Link>
+
+          <div className="hidden ">
+            <div>
+              <div className="flex flex-col justify-center items-center  ">
+                <Link href={"/george"} className="menuitems">
+                  <MenuButton>Georges</MenuButton>
+                </Link>
+                <Link href={"/lace"} className="menuitems">
+                  <MenuButton>Laces</MenuButton>
+                </Link>
+                <Link href={"/bag"} className="menuitems">
+                  <MenuButton>Bags</MenuButton>
+                </Link>
+              </div>
+              <div className="flex flex-col justify-center items-center  ">
+                <Link href={"/jewelry"} className="menuitems">
+                  <MenuButton>Jewelries</MenuButton>
+                </Link>
+                <Link href={"/headtie"} className="menuitems">
+                  <MenuButton>Headtie Gele</MenuButton>
+                </Link>
+                <Link href={"/accessories"} className="menuitems">
+                  <MenuButton>Accessories</MenuButton>
+                </Link>
+              </div>
+            </div>
+            <div>
+              <div className="flex flex-col justify-center items-center">
+                <Link href={"/slippers"} className="menuitems">
+                  <MenuButton>Slippers</MenuButton>
+                </Link>
+                <Link href={"/ankara"} className="menuitems">
+                  <MenuButton>Ankara</MenuButton>
+                </Link>
+                <Link href={"/shoe"} className="menuitems">
+                  <MenuButton>Shoes</MenuButton>
+                </Link>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <Link href={"/men"} className="menuitems">
+                  <MenuButton>Men Shop</MenuButton>
+                </Link>
+                <Link href={"/clutch"} className="menuitems">
+                  <MenuButton>Clutches</MenuButton>
+                </Link>
+                <Link href={"/dress"} className="menuitems">
+                  <MenuButton>Gown Dresses</MenuButton>
+                </Link>
+              </div>
+            </div>
           </div>
           <SocialMedia />
         </div>
